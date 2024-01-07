87d60eae280b5015f7e899673e66b10ea7d911f1
bushm <anton.gorbovyi@gmail.com>
2024-01-08 00:14:59 +0100

    Homework of Anton G.

---
+++ src/hw01/Task2.java
@@ -0,0 +1,22 @@
+package hw01;
+
+public class Task2 {
+    public static void main(String[] args){
+        byte x = 4;
+        short y = 56;
+        int z = 89;
+        long i = 12121;
+        float n = 4.7333436f;
+        double k = 4.355453532;
+        char a = 'G';
+
+        System.out.println("Result for byte: " + x);
+        System.out.println("Result for short: " + y);
+        System.out.println("Result for int: " + z);
+        System.out.println("Result for long: " + i);
+        System.out.println("Result for float: " + n);
+        System.out.println("Result for double: " + k);
+        System.out.println("Result for char: " + a);
+
+    }
+}
