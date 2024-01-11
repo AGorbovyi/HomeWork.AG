4329baea31a4c8e83917cf39c193e031c01e9379
bushm <anton.gorbovyi@gmail.com>
2024-01-11 22:43:29 +0100

    Homework from 2 to 21

--- src/hw03/Task2.java
+++ src/hw03/Task2.java
@@ -2,5 +2,36 @@ package hw03;
 
 public class Task2 {
 
+    public static void main(String[] args) {
+        int number1 = 0;
+        int number2 = 1;
+        int number3 = 2;
+        int number4 = 3;
+        int number5 = 4;
+        int number6 = 5;
+        int number7 = 6;
+        int number8 = 7;
+        int number9 = 8;
+        int number10 = 9;
+
+        int sum;
+        int averageNumber;
+        float remainder;
+        float tempNumber;
+
+        sum = (number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10);
+        averageNumber = sum / 10;
+        tempNumber = sum;
+        remainder = tempNumber / 10 - averageNumber;
+
+        System.out.println("Данные переменные: " + number1 + ", " + number2 + ", " + number3 + ", " + number4 + ", " + number5 + ", " + number6 + ", " + number7 + ", " + number8 + ", " + number9 + ", " + number10);
+        System.out.println("Cумма: " + sum);
+        System.out.println("Cреднее арифметическое значение всех переменных: " + averageNumber);
+        System.out.println("Остаток при делении: " + remainder);
+
+
+
+    }
+
 
 }
--- src/hw03/Task3.java
+++ src/hw03/Task3.java
@@ -1,6 +1,28 @@
 package hw03;
 
 public class Task3 {
+    public static void main(String[] args) {
+
+        float productPriceA;
+        float productPriceB;
+        float discount;
+        float orderPrice;
+        float totalOrderPrice;
+
+        productPriceA = 1000;
+        productPriceB = 500;
+        discount = 100;
+
+        orderPrice = Float.sum(productPriceA, productPriceB);
+        totalOrderPrice = orderPrice - discount;
+
+        System.out.println("Цена товара A: " + productPriceA + " руб");
+        System.out.println("Цена товара B: " + productPriceB + " руб");
+        System.out.println("Стоимость покупки: " + totalOrderPrice + " руб");
+        System.out.println("Скидка на данную покупку составила: " + discount + " руб");
+        System.out.println("Спасибо за покупку в нашем магазине!");
+
+    }
 
 
 }
